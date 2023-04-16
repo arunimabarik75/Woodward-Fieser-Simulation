4325e5014f4bb547b319812e6123424adca5668e
Arunima Barik <arunimabarik75@gmail.com>
2023-04-16 11:31:25 +0530

    second commit

---
+++ woodward-fieser/src/pages/Aim.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Aim() {
+  return (
+    <div className='experiment-page'>
+      
+    </div>
+  )
+}
--- woodward-fieser/src/pages/MainPage.js
+++ woodward-fieser/src/pages/MainPage.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './MainPage.css'
-import AuthButton from '../components/AuthButton.js'
+import Button from 'react-bootstrap/Button';
 
 export default function MainPage() {
     return (
         <div className='main-page-background'>  
             <div className='title'>
                 <h1>Determination of λmax using Woodward Fieser Rule</h1>
-                <AuthButton />
+                <Button variant="outline-primary">Login</Button>&nbsp;&nbsp;&nbsp;&nbsp;
+                <Button variant="outline-primary">Register</Button>
         </div>
         </div>
   )
